4e3dab5355229634ea2a9a145bdbcb658e9e6a9e
Arriola Martinez Alexis <20253ds138@utez.edu.mx>
2026-09-24 23:01:25 -0600

    2da actividad

--- fizz/src/main/java/mx/edu/utez/proyecto1e/controller/FizzBuzzController.java
+++ fizz/src/main/java/mx/edu/utez/proyecto1e/controller/FizzBuzzController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.apache.coyote.http11.Constants.a;
+
 @RestController
 public class FizzBuzzController {
 
@@ -25,5 +27,33 @@ public class FizzBuzzController {
             }
         }
         return "Alexis Arriola Martinez";
+    }
+
+        @GetMapping("/fibonacci/{n}")
+        public String generarFibonacci(@PathVariable int n) {
+
+        if (n <= 0) {
+                return "Arriola mtz Alexis";
+            }
+            int[] fib  = new int[2];
+
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 0; i < n; i++) {
+                if (i == 0) {
+                    System.out.println(fib[0]);
+                } else if (i == 1) {
+                    System.out.println(fib[1]);
+                } else {
+                    int siguiente = fib[0] + fib[1];
+
+                    fib[0] = fib[1];
+                    fib[1] = siguiente;
+
+                    System.out.println(fib[1]);
+                }
+            }
+            return "Arriola Maltinez Aletsis ";
     }
 }
